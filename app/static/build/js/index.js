244a0bf4e9a4f1495c153540a39cc5e498381fe7
nextvega <nextvega@nextvegasolutions.com>
2024-06-19 23:32:00 -0600

    change index page 2

--- app/static/build/js/index.js
+++ app/static/build/js/index.js
@@ -14,4 +14,26 @@ document.addEventListener("DOMContentLoaded", function() {
     //         isScrolling = false;
     //     }, 100);
     // });
+
+    let galeryTools = document.querySelectorAll(".galery__tools__grid__body__list__item") 
+
+    galeryTools.forEach((i, index) => {
+        if (index === 1) {
+            i.style.setProperty('--pseudo-background-color', `linear-gradient(to top, rgba(0, 0, 0, 0.555), rgba(0, 0, 0, 0)), url("https://images.hipdf.com/images2024/homepage/pic-person-${index+1}.png"`);
+        }
+        i.addEventListener('click', function(e){
+
+            if (index === 1) {
+                galeryTools[1].classList.remove('galery__tools__grid__body__list__item-active');
+            }
+
+            galeryTools.forEach(item => {
+                item.classList.remove('galery__tools__grid__body__list__item-active');
+            });
+
+            
+            i.classList.add('galery__tools__grid__body__list__item-active');
+            i.style.setProperty('--pseudo-background-color', `linear-gradient(to top, rgba(0, 0, 0, 0.555), rgba(0, 0, 0, 0)), url("https://images.hipdf.com/images2024/homepage/pic-person-${index+1}.png"`);
+        })
+    })
 });
